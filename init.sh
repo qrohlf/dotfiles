15a75d324c04494e45c6ce72c9cab227e213252f
Quinn Rohlf <qr@qrohlf.com>
2014-03-14 09:24:25 -0700

    fix path

--- init.sh
+++ init.sh
@@ -6,3 +6,9 @@ for i in .dotfiles/functions/*.sh ; do
         . $i
     fi
 done
+
+# Handy aliases
+alias phpserver="php -S 0.0.0.0:8001" # PHP development server on localhost:8001 - DANGER WILL ROBINSON
+alias disapprove=~/.disapprove.sh # uselessness
+alias gh="hub browse" # open github for the current repo
+alias ls="ls -G" # colored ls output
