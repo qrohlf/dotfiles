3b147d279c301f27a2023b26fc398e1df930ad71
Quinn Rohlf <qr@qrohlf.com>
2014-03-17 16:03:22 -0700

    add url for ಠ_ಠ

--- init.sh
+++ init.sh
@@ -9,6 +9,6 @@ done
 
 # Handy aliases
 alias phpserver="php -S 0.0.0.0:8001" # PHP development server on localhost:8001 - DANGER WILL ROBINSON
-alias disapprove=~/.disapprove.sh # uselessness
+alias disapprove=~/.disapprove.sh # uselessness (github.com/qrohlf/look-of-disapproval)
 alias gh="hub browse" # open github for the current repo
 alias ls="ls -G" # colored ls output
