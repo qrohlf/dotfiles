bcaa57eb98d3d68df0e658bbe066aa3c20bac028
Quinn Rohlf <qr@qrohlf.com>
2014-03-17 16:11:56 -0700

    simplify password generator

--- functions/gimmepassword.sh
+++ functions/gimmepassword.sh
@@ -2,5 +2,5 @@
 
 # generate a random password - default length is 16, can be specified as an argument
 gimmepassword() {
-    cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-16} | head -n 1
+    LC_CTYPE=C < /dev/urandom tr -dc [:alnum:] | head -c ${1:-16}; echo 
 }
