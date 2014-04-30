2b5229d57196bb4a62c924261110f4c5ee6d2ba1
Quinn Rohlf <qr@qrohlf.com>
2014-04-30 11:53:41 -0700

    new booyah functionality

--- functions/booyah.sh
+++ functions/booyah.sh
@@ -13,35 +13,60 @@
 booyah() {
     TITLE=${PWD##*/}
     VERBOSE=true    # enable notifications by default
+    UPLOAD=true     # enable push to origin by default
     BROWSE=false    # disable browse by default
     DEPLOY=false    # disable deploy by default
     local OPTIND    # http://stackoverflow.com/questions/16654607/using-getopts-inside-a-bash-function
-    while getopts "qdb" flag
+    while getopts "qdbx" flag
     do
         case $flag in 
             q) VERBOSE=false;;
             d) DEPLOY=true;;
             b) BROWSE=true;;
+            x) UPLOAD=false;;
         esac
     done
 
-    if git push
-    then
-        if [ "$VERBOSE" = true ]; then notify "$TITLE" 'git push successful'; fi
-        if [ "$BROWSE" = true ]; then hub browse; fi
-    else
-        if [ "$VERBOSE" = true ]; then notify "$TITLE" 'git push unsuccessful'; fi
-        return 1
+    if [ "$UPLOAD" = true ]; then
+        if git push; then
+            if [ "$VERBOSE" = true ]; then notify "$TITLE" 'git push successful'; fi
+        else
+            if [ "$VERBOSE" = true ]; then notify "$TITLE" 'git push unsuccessful'; fi
+            return 1
+        fi
     fi
 
     if [ "$DEPLOY" = true ]; then
-        if git push heroku master
-        then
-            if [ "$VERBOSE" = true ]; then notify "$TITLE" 'Heroku deploy successful'; fi
-            if [ "$BROWSE" = true ]; then heroku open; fi
+        DEPLOY_SUCCESS=false
+
+        # run the custom deploy script if it exists
+        if [ -a .shipit ]; then
+            if bash .shipit; then
+                DEPLOY_SUCCESS=true
+            fi
+        # otherwise assume heroku
+        else 
+            if git push heroku master; then
+                DEPLOY_SUCCESS=true
+            fi
+        fi
+
+
+        if [ "$DEPLOY_SUCCESS" = true ] && [ "$VERBOSE" = true ]; then
+            notify "$TITLE" 'Deploy successful'
         else
-            if [ "$VERBOSE" = true ]; then notify "$TITLE" 'Heroku deploy unsuccessful'; fi
+            notify "$TITLE" 'Deploy unsuccessful'
             return 1
+        fi
+    fi
+
+    if [ "$BROWSE" = true ]; then
+        if [ -a .shipit-browse ]; then 
+            open `cat .shipit-browse`; 
+        elif heroku info &> /dev/null; then
+            heroku open
+        elif hub browse -u; then
+            hub browse
         fi
     fi
 }
--- init.sh
+++ init.sh
@@ -12,4 +12,5 @@ alias phpserver="php -S 0.0.0.0:8001" # PHP development server on localhost:8001
 alias disapprove=~/.disapprove.sh # uselessness (github.com/qrohlf/look-of-disapproval)
 alias gh="hub browse" # open github for the current repo
 alias ls="ls -G" # colored ls output
-alias duh="du -sh"
+alias duh="du -sh" # quick file/directory size
+alias gitroot='cd $(git rev-parse --show-cdup)' # quickly cd to the repo root
