bfc5a7b6eec1bccfa7a941547fd79f0e13b4d9ab
Quinn Rohlf <qr@qrohlf.com>
2014-05-03 08:50:50 -0700

    gimmepassword: only output newline if terminal

--- functions/gimmepassword.sh
+++ functions/gimmepassword.sh
@@ -2,5 +2,6 @@
 
 # generate a random password - default length is 16, can be specified as an argument
 gimmepassword() {
-    LC_CTYPE=C < /dev/urandom tr -dc [:alnum:] | head -c ${1:-16}; echo 
+    LC_CTYPE=C < /dev/urandom tr -dc [:alnum:] | head -c ${1:-16} #password
+    if [ -t 1 ]; then echo; fi #newline if terminal
 }
