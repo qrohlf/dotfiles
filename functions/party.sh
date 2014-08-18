d4e6ed7925221ae5b74420227badb96a2b32f506
Quinn Rohlf <qr@qrohlf.com>
2014-08-17 18:44:14 -0700

    fix math for :rainbow:

--- functions/party.sh
+++ functions/party.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 # Party Terminal
-# from http://rcr.io/words/dynamic-xterm-colors.html
+#
+# credit goes to http://rcr.io/words/dynamic-xterm-colors.html
+# OSX port by @qrohlf (http://qrohlf.com)
 
 party() {
   A=0;
-	F="0.1"
+  F="0.1"
 
-	while true; do
+  while true; do
+    [ $A == 628318 ] && A=0 || A=$((A + 1))
+    R=$(echo "s ($F*$A + 0)*32767 + 32768" | bc -l | cut -d'.' -f1)
+    B=$(echo "s ($F*$A + 2)*32767 + 32768" | bc -l | cut -d'.' -f1)
+    G=$(echo "s ($F*$A + 4)*32767 + 32768" | bc -l | cut -d'.' -f1)
 
-	[ $A == 628318 ] && A=0 || A=$((A + 1))
+    osascript -e " \
+    tell application \"Terminal\"
+    set normal text color of window 1 to {$R, $G, $B}
+    set cursor color of window 1 to {$R, $G, $B}
+    end tell"
 
-	R=$(echo "s ($F*$A + 0)*32767 + 32767" | bc -l | cut -d'.' -f1)
-	B=$(echo "s ($F*$A + 2)*32767 + 32767" | bc -l | cut -d'.' -f1)
-	G=$(echo "s ($F*$A + 4)*32767 + 32767" | bc -l | cut -d'.' -f1)
-
-  osascript -e " \
-  tell application \"Terminal\"
-  set normal text color of window 1 to {$R, $G, $B}
-  set cursor color of window 1 to {$R, $G, $B}
-  end tell"
-
-	sleep 0.01
+    sleep 0.01
   done &
 }
-
-# set_foreground() {
-#   R=
-#   osascript -e " \
-#   tell application \"Terminal\"
-#   set normal text color of window 1 to {$R, $G, $B}
-#   end tell"
-#   #set cursor color to {@$1} \
-# }
