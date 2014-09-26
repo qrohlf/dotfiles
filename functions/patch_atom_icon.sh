fd8d889e7f8620136baac7a34c5b4ed8d3946d49
Quinn Rohlf <qr@qrohlf.com>
2014-09-26 15:35:57 -0700

    atom icon patch

---
+++ functions/patch_atom_icon.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+patch_atom_icon() {
+  RES="${BASH_SOURCE%/*}/../resources"
+  cp -f "$RES/atom-yosemite.icns" /Applications/Atom.app/Contents/Resources/atom.icns
+  #sudo find /private/var/folders -name com.apple.dock.iconcache -exec rm {} \; # probably not needed
+  #sudo find /private/var/folders -name com.apple.iconservices -exec rm -rf {} \; # probably not needed
+  #sudo mv /Library/Caches/com.apple.iconservices.store com.apple.iconservices.backup # Almost definitely not needed
+  echo "Now reboot."
+}
