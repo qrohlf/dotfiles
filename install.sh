2fbeac44283ad0314c02d79f38e67f670fa03995
Quinn Rohlf <qr@qrohlf.com>
2014-10-07 09:14:00 -0700

    rename sublime folder

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/.dotfiles/.gemrc ~/.gemrc
 ln -s ~/.dotfiles/.irbrc ~/.irbrc
 ln -s ~/.dotfiles/.sqliterc ~/.sqliterc
-ln -s ~/.dotfiles/Sublime ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -s ~/.dotfiles/sublime ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
