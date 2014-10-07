bc625aa7902c1be6745c4c4c2e636936ceb8894e
Quinn Rohlf <qr@qrohlf.com>
2014-10-07 14:05:51 -0700

    add scripts

---
+++ scripts/Readme.md
@@ -0,0 +1,3 @@
+# Scripts
+
+This is for one-off "snippet" scripts that are only useful occasionally. They are not loaded automatically by init.
---
+++ scripts/symlink_sublime.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# simple script to symlink the subl executable into /usr/local
+
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
