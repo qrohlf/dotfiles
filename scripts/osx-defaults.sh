e8e31de560605b0364e4970dc50fd953dc90b650
Quinn Rohlf <qr@qrohlf.com>
2015-08-12 11:01:31 -0700

    osx defaults tweaks add a new iterm icon

--- scripts/osx-defaults.sh
+++ scripts/osx-defaults.sh
@@ -6,5 +6,10 @@ defaults write com.apple.screencapture location ~/Downloads
 defaults write com.apple.screencapture disable-shadow -bool true
 # Enable text selection in Quick Look
 defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+# Kill finder with cmd+q
+defaults write com.apple.finder NSUserKeyEquivalents -dict-add "Close All" -string "@q"
 # Apply changes
 killall SystemUIServer Finder
