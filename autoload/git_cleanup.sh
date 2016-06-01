787b5c0c657874bdd505ad3f65cc53ca0982413d
Quinn Rohlf <qr@qrohlf.com>
2016-06-01 10:50:22 -0700

    Clean up some autojump output. Change `git-cleanup` to `git cleanup`

--- autoload/git_cleanup.sh
+++
@@ -1,4 +0,0 @@
-git-cleanup() {
-  git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d
-  git remote prune `git remote`
-}
--- autoload/r.sh
+++ autoload/r.sh
@@ -1,5 +1,5 @@
 r() {
   cd /Users/qrohlf/Code/ridewithgps/ridewithgps
   /Users/qrohlf/Code/ridewithgps/ridewithgps/ride.rb $@
-  cd -
+  cd - &> /dev/null
 }
