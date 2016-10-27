525f0e7e6f50ae480b506f64fc4169c92db6fb40
Quinn Rohlf <qr@qrohlf.com>
2016-10-27 09:21:36 -0700

    screengif.sh: allow rescale

--- bin/screengif.sh
+++ bin/screengif.sh
@@ -2,7 +2,9 @@
 
 palette="/tmp/palette.png"
 
-filters="fps=15,scale=650:-1:flags=lanczos"
+scale=${3:-"650"}
+
+filters="fps=15,scale=$scale:-1:flags=lanczos"
 
 ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
 ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
