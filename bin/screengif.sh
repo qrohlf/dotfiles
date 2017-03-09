39cb063d2731fbe8acaa524620850bac4adbef5d
Quinn Rohlf <qr@qrohlf.com>
2017-03-09 09:03:41 -0800

    use subl for git commits

--- bin/screengif.sh
+++ bin/screengif.sh
@@ -5,13 +5,12 @@ palette="/tmp/palette.png"
 scale=${3:-"650"}
 
 filters="fps=15,scale=$scale:-1:flags=lanczos"
+# use filters="fps=15" for no rescale
 
-ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $1 -vf "palettegen" -y $palette
 ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
 echo "unoptimized file size:"
 du -sh $2
 echo "optimizing:"
-FILENAME=${FILE##*/}
-FILEPATH=${FILE%/*}
-NOEXT=${FILENAME%\.*}
-gifisicle -O3 $2 "$FILEPATH/$FILENAME-opti.gif"
+gifsicle -O3 $2 > $2.opti
+du -sh $2.opti
