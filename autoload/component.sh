cea326c073a1ea5334a1b0a35f0e3b31c31f16a8
Quinn Rohlf <qr@qrohlf.com>
2018-01-03 12:04:22 -0800

    refactor component.sh to use es6 classes

--- autoload/component.sh
+++ autoload/component.sh
@@ -4,10 +4,15 @@ cat << EOM > $1/$1.js
 import React from 'react'
 import styles from './$1.scss'
 
-export const $1 = () => (
-  <div className={styles.$1}>
-  </div>
-)
+export class $1 extends React.PureComponent {
+  render () {
+    return (
+      <div className={styles.$1}>
+        {'<$1 />'}
+      </div>
+    )
+  }
+}
 EOM
 
 cat << EOM > $1/$1.test.js
