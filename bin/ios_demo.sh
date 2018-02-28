f03b8a98857834daaad68aea40189fb0634d12ec
Quinn Rohlf <qr@qrohlf.com>
2018-02-28 10:56:47 -0800

    alias git to g, add iphone 8 info to ios demo.sh

--- bin/ios_demo.sh
+++ bin/ios_demo.sh
@@ -1,6 +1,6 @@
 # some helpful filters (-vf $FOOBAR)
-# iphone 6 videos
-# SCALE=240:-1
+# iphone 8 videos
+# SCALE=242:-1
 # android nexus videos
 # SCALE=242:-1
 # crop dumb keynote black bars for garmin
@@ -9,5 +9,5 @@
 # install: brew install ffmpeg --with-libvpx
 
 # SCALE=450:-1
-ffmpeg -i $1 -an -vf scale=450:-1 -c:v libvpx -crf 10 -b:v 1M $2.webm
-ffmpeg -i $1 -an -vf scale=450:-1 -vcodec h264 -strict -2 $2.mp4
+ffmpeg -i $1 -an -vf scale=242:-1 -c:v libvpx -crf 10 -b:v 1M $1.webm
+ffmpeg -i $1 -an -vf scale=242:-1 -vcodec h264 -strict -2 $1.mp4
